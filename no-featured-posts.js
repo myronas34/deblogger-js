ac513ff56e3995d62c0d5d1f973eea02dd43ce21
myronas34 <43584237+myronas34@users.noreply.github.com>
2022-12-01 12:19:14 +0000

    Update no-featured-posts.js

--- no-featured-posts.js
+++ no-featured-posts.js
@@ -2,7 +2,7 @@
 let isPost;
 let FeaturedPosts;
 isPost = document.getElementsByClassName("post-view");
-  if (isPost > 0) {
+  if (isPost = 0) {
     FeaturedPosts = document.getElementsByClassName("FeaturedPost");
     for (let i = 0; i < Features.length; i++) {
       Features[i].style.display = "none";
